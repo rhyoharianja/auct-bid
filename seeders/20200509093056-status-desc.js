a0e7ab72fe78a85e2b5f69b2b339d5f82de8a894
suryo-harianja <suryo@aleph-labs.com>
2020-05-10 22:50:49 +0700

    default valuse status

--- seeders/20200509093056-status-desc.js
+++ seeders/20200509093056-status-desc.js
@@ -6,43 +6,57 @@ module.exports = {
         statusType: 'payment',
         statusCode: 10,
         statusName: 'Waiting Payment',
-        statusDesc: 'Waiting Payment'
+        statusDesc: 'Waiting Payment',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         statusType: 'payment',
         statusCode: 11,
         statusName: 'Payment Confirmation',
-        statusDesc: 'Payment Confirmation'
+        statusDesc: 'Payment Confirmation',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         statusType: 'payment',
         statusCode: 12,
         statusName: 'Payment Expired',
-        statusDesc: 'Payment Expired'
+        statusDesc: 'Payment Expired',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         statusType: 'order',
         statusCode: 20,
         statusName: 'On Order',
-        statusDesc: 'Payment Confirmation'
+        statusDesc: 'Payment Confirmation',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         statusType: 'order',
         statusCode: 21,
         statusName: 'Order Confirmed',
-        statusDesc: 'Order Confirmed'
+        statusDesc: 'Order Confirmed',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         statusType: 'order',
         statusCode: 22,
         statusName: 'Order On Delivery',
-        statusDesc: 'Order On Delivery'
+        statusDesc: 'Order On Delivery',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         statusType: 'order',
         statusCode: 23,
         statusName: 'Order Finished',
-        statusDesc: 'Order Finished'
+        statusDesc: 'Order Finished',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
     ]);
   },
